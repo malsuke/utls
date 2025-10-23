ae1dcb1838145a731e5d0aea26c2ed696e57c557
malsuke <malsuke096@gmail.com>
2025-10-23 12:18:47 +0900

    update

---
+++ out/_next/static/312jTkmG2SldKSyu4g_CW/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/ed6e0bb1fae2bb59.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
